00db4157c27ec5d8c8a007e3d353ed24ccc98514
TrejGun <trejgun@gmail.com>
2023-01-31 12:25:35 +0800

    feat: update solidity 8.9 -> 8.13

--- contracts/upgradeable/contracts/UUPS.sol
+++ contracts/upgradeable/contracts/UUPS.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
